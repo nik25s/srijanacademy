e9978262a74920429566120324342241998df4b5
Subin <subinsk284@gmail.com>
2021-06-30 21:02:22 +0530

    Procfile

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const path = require("path");
 const app = express();
-const port = 80;
-// const port = process.env.PORT || 3000
+// const port = 80;
+const port = process.env.PORT || 3000
 
 // app.use('static', express.static('static'))
 
